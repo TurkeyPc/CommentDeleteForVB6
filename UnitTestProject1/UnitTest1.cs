e9adaaba6431b66e0c6c250259e4d0491c71aab4
TurkeyPc <takkun.iyama@nifty.com>
2016-11-05 09:07:52 +0900

    Add UnitTest For Combination LogicalRows And DeleteComment2

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -136,5 +136,59 @@ namespace UnitTestProject1
             Assert.AreEqual("Dim v As Variant", actual[2][0]);
             Assert.AreEqual("End Sub", actual[3][0]);
         }
+
+        [TestMethod]
+        public void TestMethodCombo1()
+        {
+            var s = Properties.Resources.TestMethod9InputValue;
+            var ss = s.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var actual = Program.LogicalRows(ss).Select(p => Program.DeleteComment2(p)).ToArray();
+
+            Assert.AreEqual(3, actual.Count());
+            Assert.AreEqual(1, actual[0].Count());
+            Assert.AreEqual(1, actual[1].Count());
+            Assert.AreEqual(1, actual[2].Count());
+
+            Assert.AreEqual("Private Sub Form_Load()", actual[0].ToArray()[0]);
+            Assert.AreEqual("", actual[1].ToArray()[0]);
+            Assert.AreEqual("End Sub", actual[2].ToArray()[0]);
+        }
+
+        [TestMethod]
+        public void TestMethodCombo2()
+        {
+            var s = Properties.Resources.TestMethodAInputValue;
+            var ss = s.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var actual = Program.LogicalRows(ss).Select(p => Program.DeleteComment2(p)).ToArray();
+
+            Assert.AreEqual(4, actual.Count());
+            Assert.AreEqual(1, actual[0].Count());
+            Assert.AreEqual(1, actual[1].Count());
+            Assert.AreEqual(1, actual[2].Count());
+            Assert.AreEqual(1, actual[3].Count());
+
+            Assert.AreEqual("Private Sub Form_Load()", actual[0].ToArray()[0]);
+            Assert.AreEqual("", actual[1].ToArray()[0]);
+            Assert.AreEqual("Dim v As Variant", actual[2].ToArray()[0]);
+            Assert.AreEqual("End Sub", actual[3].ToArray()[0]);
+        }
+
+
+        [TestMethod]
+        public void TestMethodCombo3()
+        {
+            var s = Properties.Resources.TestMehtod8InputValue;
+            var ss = s.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var actual = Program.LogicalRows(ss).Select(p => Program.DeleteComment2(p)).ToArray();
+
+            Assert.AreEqual(1, actual.Count());
+            Assert.AreEqual(2, actual[0].Count());
+
+            Assert.AreEqual("Dim s As _", actual[0].ToArray()[0]);
+            Assert.AreEqual("String ", actual[0].ToArray()[1]);
+        }
     }
 }

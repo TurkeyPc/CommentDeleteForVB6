378e3f8d003fdd1a5b89e48175771276935fc07c
TurkeyPc <takkun.iyama@nifty.com>
2016-11-05 12:02:23 +0900

    Add MessageBox

--- CommentDeleteForVB6/Form1.cs
+++ CommentDeleteForVB6/Form1.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Linq;
 
 namespace CommentDeleteForVB6
 {
@@ -16,12 +17,17 @@ namespace CommentDeleteForVB6
         {
             OpenFileDialog v = new OpenFileDialog();
 
-            if (v.ShowDialog() == DialogResult.OK)
-            {
-                var vb6module = new VB6Source(v.FileName);
-                File.WriteAllLines(v.FileName, vb6module.CommentDeleted,Encoding.Default);
-            }
+            v.Title = "Select VB6 Module File";
 
+            if (v.ShowDialog() != DialogResult.OK) return;
+
+            if (MessageBox.Show("Selected file will be overwritten!" + Environment.NewLine + "Are you OK?", "Caution", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) != DialogResult.Yes)
+                return;
+
+            var vb6module = new VB6Source(v.FileName);
+            File.WriteAllLines(v.FileName, vb6module.CommentDeleted.Where(p => p.Trim() != ""), Encoding.Default);
         }
+
     }
 }
+

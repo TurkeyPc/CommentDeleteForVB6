f3cb31662008e8364529ab63580f0dbf6e008fe0
TurkeyPc <takkun.iyama@nifty.com>
2016-11-05 10:31:32 +0900

    Add a method in VB6Source

--- CommentDeleteForVB6/VB6Source.cs
+++ CommentDeleteForVB6/VB6Source.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.IO;
+
 
 namespace CommentDeleteForVB6
 {
     class VB6Source
     {
+        private readonly string[] source;
+
+        public VB6Source(string path)
+        {
+            this.source = File.ReadAllLines(path);
+        }
+
+        public VB6Source(IEnumerable<string> source)
+        {
+            this.source = source.ToArray();
+        }
+
+        public IEnumerable<string> CommentDeleted
+        {
+            get
+            {
+                return null;
+            }
+        }
+
     }
 }

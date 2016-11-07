b985604518924200e68e2463d8e4d6c1a4ef1aef
TurkeyPc <takkun.iyama@nifty.com>
2016-11-07 10:53:45 +0900

    Add Select Folder Button

--- CommentDeleteForVB6/Form1.cs
+++ CommentDeleteForVB6/Form1.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CommentDeleteForVB6
 {
@@ -24,8 +25,36 @@ namespace CommentDeleteForVB6
             if (MessageBox.Show("Selected file will be overwritten!" + Environment.NewLine + "Are you OK?", "Caution", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) != DialogResult.Yes)
                 return;
 
-            var vb6module = new VB6Source(v.FileName);
-            File.WriteAllLines(v.FileName, vb6module.CommentDeleted.Where(p => p.Trim() != ""), Encoding.Default);
+            DoDeleteComent(v.FileName);
+        }
+
+        private static void DoDeleteComent(string s)
+        {
+            var vb6module = new VB6Source(s);
+            File.WriteAllLines(s, vb6module.CommentDeleted.Where(p => p.Trim() != ""), Encoding.Default);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog v = new FolderBrowserDialog();
+
+            v.Description = "Select VB6 Module Folder";
+
+            if (v.ShowDialog() != DialogResult.OK) return;
+
+            if (MessageBox.Show("Selected file in folder will be overwritten!" + Environment.NewLine + "Are you OK?", "Caution", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) != DialogResult.Yes)
+                return;
+
+            var exts = new[] {"bas","frm","cls" };
+
+            foreach (var ext in exts)
+            {
+                foreach (var f in Directory.GetFiles(v.SelectedPath, "*." + ext))
+                {
+                    DoDeleteComent(f);
+                }
+
+            }
         }
 
     }
